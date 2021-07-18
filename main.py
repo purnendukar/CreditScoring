56702dc673e1766c40e9f63398b9302fa3f1f727
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-07-18 11:40:48 +0530

    server port change

--- main.py
+++ main.py
@@ -145,5 +145,5 @@ def feedback_loop(data: List[FeedbackIn]):
     return {"detail": "Feedback loop successful"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8888, reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
 
